eb4dcb683cda2d7be4b2475ab9be0d9ff68364d4
eldaicho72 <jesusdelg2000@gmail.com>
2025-07-03 13:18:06 -0400

    app de vuelta

--- src/components/header/AppHeaderDropdown.js
+++ src/components/header/AppHeaderDropdown.js
@@ -9,22 +9,10 @@ import {
   CDropdownMenu,
   CDropdownToggle,
 } from '@coreui/react'
-import {
-  cilBell,
-  cilCreditCard,
-  cilCommentSquare,
-  cilEnvelopeOpen,
-  cilFile,
-  cilLockLocked,
-  cilSettings,
-  cilTask,
-  cilUser,
-
-} from '@coreui/icons'
+import { cilAccountLogout, cilSettings } from '@coreui/icons'
 import CIcon from '@coreui/icons-react'
 
-
-import avatar8 from './../../assets/images/avatars/7.png'
+import avatar8 from './../../assets/images/avatars/8.jpg'
 
 const AppHeaderDropdown = () => {
   return (
@@ -33,29 +21,26 @@ const AppHeaderDropdown = () => {
         <CAvatar src={avatar8} size="md" />
       </CDropdownToggle>
       <CDropdownMenu className="pt-0" placement="bottom-end">
-       
-        <CDropdownHeader className="bg-body-secondary fw-semibold my-2">Cuenta</CDropdownHeader>
-        <CDropdownItem href="#/profile" >
-          
-          <CIcon icon={cilUser} className="me-2" />
-          Mi Perfil
-        </CDropdownItem>
+        <CDropdownHeader className="bg-body-secondary fw-semibold my-2">
+          Configuracion
+        </CDropdownHeader>
+
         <CDropdownItem href="#">
           <CIcon icon={cilSettings} className="me-2" />
           Ajustes
         </CDropdownItem>
-        <CDropdownItem href="#">
-          <CIcon icon={cilCreditCard} className="me-2" />
-          Pagos
-          <CBadge color="secondary" className="ms-2">
-            
-          </CBadge>
-        </CDropdownItem>
-        
+
         <CDropdownDivider />
-        <CDropdownItem href="#">
-          <CIcon icon={cilLockLocked} className="me-2" />
-          Cerar sesión
+        <CDropdownItem
+          href="#"
+          onClick={() => {
+            localStorage.removeItem('token')
+            localStorage.removeItem('usuario')
+            window.location.reload()
+          }}
+        >
+          <CIcon icon={cilAccountLogout} className="me-2" />
+          Log out
         </CDropdownItem>
       </CDropdownMenu>
     </CDropdown>
